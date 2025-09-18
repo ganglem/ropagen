aaf5fbfd0ff18b7183035855095bf5e0ffac5f7d
Emilija Kastratović <emilija1705@gmail.com>
2025-09-19 01:16:55 +0200

    add docs framework with first example entry

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
+import { createMDX } from 'fumadocs-mdx/next';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const withMDX = createMDX({
+    // customise the config file path
+    // configPath: "source.config.ts"
+});
+
+
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+
+export default withMDX(withNextIntl(nextConfig));
