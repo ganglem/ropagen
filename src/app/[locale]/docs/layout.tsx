99b9c2e006bd187012aadf31acf13acf2a7209e7
Emilija Kastratović <emilija1705@gmail.com>
2025-09-19 14:12:52 +0200

    force dynamic

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -3,6 +3,8 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
 import {RootProvider} from "fumadocs-ui/provider";
 
+export const dynamic = 'force-dynamic';
+
 // @ts-ignore
 export default function Layout({ children }: LayoutProps<'/docs'>) {
     return <RootProvider>
