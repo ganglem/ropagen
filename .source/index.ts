6a1d0804898628a8e09c0c94113419ba1dd0adbe
emilija <emilija1705@gmail.com>
2025-10-02 22:50:16 +0200

    misc

--- .source/index.ts
+++ .source/index.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck -- skip type checking
-import * as docs_3 from "../src/content/docs/tech.mdx?collection=docs&hash=1758235725906"
-import * as docs_2 from "../src/content/docs/index.mdx?collection=docs&hash=1758235725906"
-import * as docs_1 from "../src/content/docs/howto.mdx?collection=docs&hash=1758235725906"
-import * as docs_0 from "../src/content/docs/about.mdx?collection=docs&hash=1758235725906"
+import * as docs_3 from "../src/content/docs/tech.mdx?collection=docs&hash=1759403943428"
+import * as docs_2 from "../src/content/docs/index.mdx?collection=docs&hash=1759403943428"
+import * as docs_1 from "../src/content/docs/howto.mdx?collection=docs&hash=1759403943428"
+import * as docs_0 from "../src/content/docs/about.mdx?collection=docs&hash=1759403943428"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"about.mdx","absolutePath":"/Users/ganglem/ropagen/src/content/docs/about.mdx"}, data: docs_0 }, { info: {"path":"howto.mdx","absolutePath":"/Users/ganglem/ropagen/src/content/docs/howto.mdx"}, data: docs_1 }, { info: {"path":"index.mdx","absolutePath":"/Users/ganglem/ropagen/src/content/docs/index.mdx"}, data: docs_2 }, { info: {"path":"tech.mdx","absolutePath":"/Users/ganglem/ropagen/src/content/docs/tech.mdx"}, data: docs_3 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"about.mdx","absolutePath":"C:/Dev/ropagen/src/content/docs/about.mdx"}, data: docs_0 }, { info: {"path":"howto.mdx","absolutePath":"C:/Dev/ropagen/src/content/docs/howto.mdx"}, data: docs_1 }, { info: {"path":"index.mdx","absolutePath":"C:/Dev/ropagen/src/content/docs/index.mdx"}, data: docs_2 }, { info: {"path":"tech.mdx","absolutePath":"C:/Dev/ropagen/src/content/docs/tech.mdx"}, data: docs_3 }], [])
