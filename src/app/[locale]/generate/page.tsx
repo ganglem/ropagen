5ef09b18509d0ed940be6f0e08dfdbbebb86e31b
emilija <emilija1705@gmail.com>
2025-10-23 02:32:49 +0200

    add names to modes

--- src/app/[locale]/generate/page.tsx
+++ src/app/[locale]/generate/page.tsx
@@ -16,9 +16,9 @@ export default function Generate() {
         <main className="container mx-auto py-10 relative space-y-6 w-full">
             <Tabs defaultValue="mode1" className="w-full">
                 <TabsList className="mb-4">
-                    <TabsTrigger value="mode1">Mode 1</TabsTrigger>
-                    <TabsTrigger value="mode2">Mode 2</TabsTrigger>
-                    <TabsTrigger value="mode3">Mode 3</TabsTrigger>
+                    <TabsTrigger value="mode1">Form</TabsTrigger>
+                    <TabsTrigger value="mode2">Explain</TabsTrigger>
+                    <TabsTrigger value="mode3">Chat</TabsTrigger>
                 </TabsList>
 
                 <TabsContent value="mode1" className="space-y-6">
@@ -27,12 +27,12 @@ export default function Generate() {
                 </TabsContent>
 
                 <TabsContent value="mode2" className="space-y-6">
-                    <RopaChat setGeneratedDocument={setGeneratedDocument}/>
+                    <RopaExplain setGeneratedDocument={setGeneratedDocument}/>
                     {generatedDocument != "" && <RopaPreview generatedDocument={generatedDocument} setGeneratedDocument={setGeneratedDocument}/>}
                 </TabsContent>
 
                 <TabsContent value="mode3" className="space-y-6">
-                    <RopaExplain setGeneratedDocument={setGeneratedDocument}/>
+                    <RopaChat setGeneratedDocument={setGeneratedDocument}/>
                     {generatedDocument != "" && <RopaPreview generatedDocument={generatedDocument} setGeneratedDocument={setGeneratedDocument}/>}
                 </TabsContent>
             </Tabs>
