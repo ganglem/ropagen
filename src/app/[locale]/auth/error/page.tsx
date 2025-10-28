43ebc3870553db4c834668f9aea2deb2bf2d0654
emilija <emilija1705@gmail.com>
2025-10-29 00:22:19 +0100

    fix missing comp

--- src/app/[locale]/auth/error/page.tsx
+++ src/app/[locale]/auth/error/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardTitle, CardHeader } from "@/components/ui/card";
+import {Card, CardTitle, CardHeader, CardContent} from "@/components/ui/card";
 
 export default async function Page({
   searchParams,
